7d2dba8d24ddbebf69aa4e2147b30b5073617a63
Travis Massner <tmassner@iastate.edu>
2022-09-08 13:47:04 -0500

    Login Experiment
    I added an experiement with login type of stuff

---
+++ Experiments/Travis/LoginTest/app/src/main/java/com/example/logintest/FirstFragment.java
@@ -0,0 +1,55 @@
+package com.example.logintest;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.logintest.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    public static String name;
+    public static String password;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = binding.editTextTextPersonName.getText().toString();
+                password = binding.editTextTextPassword.getText().toString();
+
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
