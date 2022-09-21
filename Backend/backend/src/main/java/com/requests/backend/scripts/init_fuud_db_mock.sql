6f561611823e8672255f116a51e720d542324834
jorgoose <your.email@goes.here>
2022-09-21 17:49:51 -0500

    Added FavoriteRepository and Favorite class. Added endpoint in UserController, "/getFavoritesByUsername/{username}", which can gather all of the favorite foods of a given user from the MySQL database.

--- Backend/backend/src/main/java/com/requests/backend/scripts/init_fuud_db_mock.sql
+++ Backend/backend/src/main/java/com/requests/backend/scripts/init_fuud_db_mock.sql
@@ -1,4 +1,3 @@
-
 -- Fast, Unique and User-Friendly Dieting!
 CREATE DATABASE IF NOT EXISTS fuud_db;
 
@@ -6,8 +5,8 @@ USE fuud_db;
 
 CREATE TABLE IF NOT EXISTS users (
     username CHAR(50) PRIMARY KEY,
-    p_hash BLOB,
-    p_salt BINARY(16),
+    p_hash VARCHAR(64),
+    p_salt CHAR(16),
     user_type CHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
@@ -17,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS fav_foods (
     username CHAR(50),
     food_name VARCHAR(255),
-    fid INT,
+    fid VARCHAR(255),
     rank_val INT
 );
 
@@ -40,8 +39,16 @@ CREATE TABLE IF NOT EXISTS user_recipes (
     carbs INT
 );
 
+-- Create user "papajohn"
 INSERT INTO users (username, p_hash, p_salt, user_type, email, full_name, age)
-VALUES ('papajohn', RANDOM_BYTES(256), RANDOM_BYTES(16), 'Admin', 'bibp@gmail.com', 'Papa Johnson', 40);
+	VALUES ('papajohn', SHA2('pizza', 64), MD5('pizza'), 'Admin', 'bibp@gmail.com', 'Papa Johnson', 40);
 
+-- Create user "uraniumlover22"
+INSERT INTO users (username, p_hash, p_salt, user_type, email, full_name, age)
+	VALUES ('uraniumlover22', SHA2('radioactive', 64), MD5('radioactive'), 'Standard', 'gone-fission@gmail.com', 'J Robert Oppenheimer', 35);
+
+-- Create favorite food entry for user "papajohn" for food ID "513fbc1283aa2dc80c00000e", food name "Ultimate Pepperoni Pizza, Epic Stuffed Crust"
+INSERT INTO fav_foods (username, food_name, fid, rank_val)
+	VALUES ('papajohn', 'Ultimate Pepperoni Pizza, Epic Stuffed Crust', '513fbc1283aa2dc80c00000e', 1)
 
 
