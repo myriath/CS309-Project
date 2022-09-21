2789d265e35565fa0f9d647447e842ea5be4e957
jorgoose <your.email@goes.here>
2022-09-21 16:25:12 -0500

    Added SQL script to initialize database and populate with mock data (for testing purposes).

---
+++ Backend/backend/src/main/java/com/requests/backend/scripts/init_fuud_db_mock.sql
@@ -0,0 +1,47 @@
+
+-- Fast, Unique and User-Friendly Dieting!
+CREATE DATABASE IF NOT EXISTS fuud_db;
+
+USE fuud_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    username CHAR(50) PRIMARY KEY,
+    p_hash BLOB,
+    p_salt BINARY(16),
+    user_type CHAR(10) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    age INT
+);
+
+CREATE TABLE IF NOT EXISTS fav_foods (
+    username CHAR(50),
+    food_name VARCHAR(255),
+    fid INT,
+    rank_val INT
+);
+
+CREATE TABLE IF NOT EXISTS food_log (
+    username CHAR(50),
+    fid VARCHAR(255),
+    quantity INT,
+    unit_type CHAR(50),
+    log_date DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS user_recipes (
+    username CHAR(50),
+    rname CHAR(50) PRIMARY KEY,
+    serving_size INT,
+    unit_type CHAR(50),
+    calories INT,
+    fat INT,
+    protein INT,
+    carbs INT
+);
+
+INSERT INTO users (username, p_hash, p_salt, user_type, email, full_name, age)
+VALUES ('papajohn', RANDOM_BYTES(256), RANDOM_BYTES(16), 'Admin', 'bibp@gmail.com', 'Papa Johnson', 40);
+
+
+
