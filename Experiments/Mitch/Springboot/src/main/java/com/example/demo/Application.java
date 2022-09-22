fae941178701c9d30989cf61178dfbb5808d4c24
mitch <wmhudson@iastate.edu>
2022-09-22 18:10:16 -0500

    Commented out broken tests
    - Need to implement something about application context?
      IDK, backend should look into it, seems useful for unit testing

--- Experiments/Mitch/Springboot/src/main/java/com/example/demo/Application.java
+++ Experiments/Mitch/Springboot/src/main/java/com/example/demo/Application.java
@@ -3,11 +3,37 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+		Integer[] integers = new Integer[] {0, 1, -9, -9, 5, 3, 10, 10, 10, 2, 4, 4, 4, 2, 5, 6, 8, 10};
+		System.out.println(sortAndRemoveDuplicates(new ArrayList<>(List.of(integers))));
+	}
+
+	public static ArrayList<Integer> sortAndRemoveDuplicates(ArrayList<Integer> nums) {
+		ArrayList<Integer> results = new ArrayList<>();
+		HashSet<Integer> set = new HashSet<>();
+		while (nums.size() > 0) {
+			int temp = nums.get(0);
+			for (int j = 0; j < nums.size(); j++) {
+				if (nums.get(j) < temp) {
+					temp = nums.get(j);
+				}
+				else if (nums.get(j) == temp && j != 0){
+					nums.remove(j);
+				}
+
+			}
+			results.add(temp);
+			nums.remove((Integer) temp);
+		}
+		return results;
 	}
 
 }
