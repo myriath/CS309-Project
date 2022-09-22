171616e885869f063546fae4e14acd4571807f0a
jorgoose <your.email@goes.here>
2022-09-22 16:39:25 -0500

    Adjust SQL script to populate MySQL DB with mock data.

--- Backend/backend/src/main/java/com/requests/backend/scripts/init_fuud_db_mock.sql
+++ Backend/backend/src/main/java/com/requests/backend/scripts/init_fuud_db_mock.sql
@@ -6,7 +6,7 @@ USE fuud_db;
 CREATE TABLE IF NOT EXISTS users (
     username CHAR(50) PRIMARY KEY,
     p_hash VARCHAR(64),
-    p_salt CHAR(16),
+    p_salt CHAR(32),
     user_type CHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
@@ -39,13 +39,13 @@ CREATE TABLE IF NOT EXISTS user_recipes (
     carbs INT
 );
 
--- Create user "papajohn"
+-- Create user "papajohn". Unhashed password is "hello".food_logfood_log
 INSERT INTO users (username, p_hash, p_salt, user_type, email, full_name, age)
-	VALUES ('papajohn', SHA2('pizza', 64), MD5('pizza'), 'Admin', 'bibp@gmail.com', 'Papa Johnson', 40);
+	VALUES ('papajohn', 'b1880571f997b2b19f66e6fedf82c19ba3be1ffc8e65c3a98f4e22ba9784c2bf', '41ccde641586d6c393c1cf5f9436564a', 'Admin', 'bibp@gmail.com', 'Papa Johnson', 40);
 
--- Create user "uraniumlover22"
+-- Create user "uraniumlover22". Unhashed password is "world".
 INSERT INTO users (username, p_hash, p_salt, user_type, email, full_name, age)
-	VALUES ('uraniumlover22', SHA2('radioactive', 64), MD5('radioactive'), 'Standard', 'gone-fission@gmail.com', 'J Robert Oppenheimer', 35);
+	VALUES ('uraniumlover22', 'a606a804cce3f7a6ea40e0fec1adf66c510df1a69511d5633639a5768b59a28c', '02047aec31856db44b5da6ec1d294393', 'Standard', 'gone-fission@gmail.com', 'J Robert Oppenheimer', 35);
 
 -- Create favorite food entry for user "papajohn" for food ID "513fbc1283aa2dc80c00000e", food name "Ultimate Pepperoni Pizza, Epic Stuffed Crust"
 INSERT INTO fav_foods (username, food_name, fid, rank_val)
