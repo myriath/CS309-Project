ec0647a113601499af6124a8e770ece719549c48
mitch <wmhudson@iastate.edu>
2022-09-26 16:54:23 -0500

    Fixing merge conflicts and replacing files from broken stuff
    Signed-off-by: mitch <wmhudson@iastate.edu>

---
+++ Frontend/app/src/main/java/com/example/cs309android/models/FDC/models/DataTypeModel.java
@@ -0,0 +1,13 @@
+package com.example.cs309android.models.FDC.models;
+
+public class DataTypeModel {
+    private final String dataType;
+
+    public DataTypeModel(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+}
