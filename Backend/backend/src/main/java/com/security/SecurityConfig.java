9bd20fad5b7fe82ff9b8ce405d901698bcad1bc3
mitch <wmhudson@iastate.edu>
2022-09-29 21:16:00 -0500

    Fiddling with https

--- Backend/backend/src/main/java/com/security/SecurityConfig.java
+++ Backend/backend/src/main/java/com/security/SecurityConfig.java
@@ -1,42 +1,41 @@
-//package com.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-///**
-// * Security config to enable HTTPS and disable CORS
-// * EXPERIMENT 5
-// *
-// * @author Mitch Hudson
-// */
-//@Configuration
-//public class SecurityConfig {
-//
-//    /**
-//     * This filters out requests, requiring HTTPS and disabling CORS and CSRF.
-//     * TODO: Stop disabling CORS and CSRF.
-//     * @param http          HTTP request
-//     * @return              Built HTTP request
-//     * @throws Exception    Errors
-//     */
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        return http.httpBasic().disable().formLogin().disable()
-////                .requiresChannel(channel ->
-////                        channel.anyRequest().requiresSecure())
-////                .authorizeRequests(authorize ->
-////                        authorize.anyRequest().permitAll())
-////                .build();
-//        http.authorizeHttpRequests(requests -> requests
-//                .antMatchers("/**").permitAll()
-//                .anyRequest().authenticated()
-//        );
-//
-//        return http.build();
-//    }
-//
-//}
+package com.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Security config to enable HTTPS and disable CORS
+ * EXPERIMENT 5
+ *
+ * @author Mitch Hudson
+ */
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * This filters out requests, requiring HTTPS and disabling CORS and CSRF.
+     * TODO: Stop disabling CORS and CSRF.
+     * @param http          HTTP request
+     * @return              Built HTTP request
+     * @throws Exception    Errors
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.httpBasic().disable().formLogin().disable()
+//                .requiresChannel(channel ->
+//                        channel.anyRequest().requiresSecure())
+//                .authorizeRequests(authorize ->
+//                        authorize.anyRequest().permitAll())
+//                .build();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
