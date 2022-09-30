f04d73bf14a90d26b1744f540c72c1c481040d0c
William Hudson <wmhudson@iastate.edu>
2022-09-30 00:38:49 +0000

    Added SecurityConfig to hopefully enable https

---
+++ Backend/backend/src/main/java/com/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * Security config to enable HTTPS and disable CORS
+ * EXPERIMENT 5
+ *
+ * @author Mitch Hudson
+ */
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * This filters out requests, requiring HTTPS and disabling CORS and CSRF.
+     * TODO: Stop disabling CORS and CSRF.
+     * @param http          HTTP request
+     * @return              Built HTTP request
+     * @throws Exception    Errors
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
