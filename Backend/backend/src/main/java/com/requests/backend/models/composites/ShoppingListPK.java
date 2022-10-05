f1bb3f9f420f6b90ca2981f3bbf5357948ee7c34
jorgoose <your.email@goes.here>
2022-10-04 20:13:50 -0500

    Changed field name in class ShoppingListPK from "itemName" to "description" to match ShoppingList.

--- Backend/backend/src/main/java/com/requests/backend/models/composites/ShoppingListPK.java
+++ Backend/backend/src/main/java/com/requests/backend/models/composites/ShoppingListPK.java
@@ -4,13 +4,15 @@ import java.io.Serializable;
 
 public class ShoppingListPK implements Serializable {
     protected String username;
-    protected String itemName;
+    protected String description;
 
     public ShoppingListPK() {}
 
-    public ShoppingListPK(String username, String itemName) {
+    public ShoppingListPK(String username, String description) {
         this.username = username;
-        this.itemName = itemName;
+        this.description = description;
     }
+
+
     // equals, hashCode
 }
