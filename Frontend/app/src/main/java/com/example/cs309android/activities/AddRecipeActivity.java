b1ca792d687839efd7511ed4a94c0ff3b7636f26
Travis Massner <tmassner@iastate.edu>
2022-10-11 03:44:47 -0500

    Added "ADD RECIPE" button as well as Snackbar and Toolbar for add recipe page

--- Frontend/app/src/main/java/com/example/cs309android/activities/AddRecipeActivity.java
+++ Frontend/app/src/main/java/com/example/cs309android/activities/AddRecipeActivity.java
@@ -1,6 +1,7 @@
 package com.example.cs309android.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,9 +13,13 @@ import com.example.cs309android.models.gson.request.recipes.AddRecipe;
 import com.example.cs309android.models.gson.response.GenericResponse;
 import com.example.cs309android.util.Constants;
 import com.example.cs309android.util.Util;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
+
+import java.util.Objects;
 
 public class AddRecipeActivity extends AppCompatActivity {
 
@@ -23,11 +28,16 @@ public class AddRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
-        Button addRecipe = findViewById(R.id.button2);
+        Button addRecipe = findViewById(R.id.add_recipe_button);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
         addRecipe.setOnClickListener(view1 -> {
-            finish();
-        });
 //            TextView RnameInput = findViewById(R.id.recipeNameInput);
 //
 //            new AddRecipe(((GlobalClass) requireActivity().getApplicationContext()).getToken(), RnameInput.getText().toString(), "put this in that and do stuff").request(response -> {
@@ -48,5 +58,22 @@ public class AddRecipeActivity extends AppCompatActivity {
 //                    recipes.setText("Something went wrong");
 //                }
 //            },getContext());
+            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.add_recipe_coordinator_layout),
+                    R.string.recipes_snackbar_added, Snackbar.LENGTH_SHORT);
+            mySnackbar.show();
+
+        });
+
+    }
+    /**
+     * Handles the back button on the toolbar
+     *
+     * @return true
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        setResult(RESULT_CANCELED);
+        finish();
+        return true;
     }
 }
