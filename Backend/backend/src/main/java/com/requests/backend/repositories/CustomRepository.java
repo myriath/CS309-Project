dade2a533a50ad2d13e2f9a2415e3c901a7917c1
jorgoose <your.email@goes.here>
2022-11-02 16:37:05 -0500

    Hotfix 1.2 for Custom Food endpoints (/get and /add).

--- Backend/backend/src/main/java/com/requests/backend/repositories/CustomRepository.java
+++ Backend/backend/src/main/java/com/requests/backend/repositories/CustomRepository.java
@@ -2,8 +2,10 @@ package com.requests.backend.repositories;
 
 import com.requests.backend.models.CustomFood;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CustomRepository extends JpaRepository<CustomFood, Integer> {
     @Query(
@@ -12,10 +14,12 @@ public interface CustomRepository extends JpaRepository<CustomFood, Integer> {
     )
     CustomFood[] queryGetCustomFoods(@Param("search") String search);
 
+    @Modifying
     @Query(
             value = "INSERT INTO custom_foods (name, name, calories, carbs, protein, fat) VALUES (:name, :calories, :carbs, :protein, :fat)",
             nativeQuery = true
     )
+    @Transactional
     void queryCreateCustomFood(@Param("name") String name, @Param("calories") double calories, @Param("carbs") double carbs, @Param("protein") double protein, @Param("fat") double fat);
 
 
