14747d2ecb192051eff6fa78c73e5e787e6e0d26
jorgoose <your.email@goes.here>
2022-11-02 10:37:08 -0500

    Hotfix for Custom Food endpoints (/get and /add).

--- Backend/backend/src/main/java/com/requests/backend/controllers/CustomController.java
+++ Backend/backend/src/main/java/com/requests/backend/controllers/CustomController.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.requests.backend.models.CustomFood;
 import com.requests.backend.models.FoodsResponse;
+import com.requests.backend.models.requests.CustomFoodRequest;
 import com.requests.backend.repositories.CustomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +19,11 @@ public class CustomController {
     private CustomRepository customRepository;
 
     @GetMapping("/get")
-    public String get(@RequestParam String search) throws JsonProcessingException {
+    public String get(@RequestParam String query) throws JsonProcessingException {
 
-        customRepository.queryGetCustomFoods(search);
-
+        customRepository.queryGetCustomFoods(query);
         FoodsResponse res = new FoodsResponse();
-        res.setItems(customRepository.queryGetCustomFoods(search));
+        res.setItems(customRepository.queryGetCustomFoods(query));
 
         // Create a new GSON Builder and disable escaping (to allow for certain unicode characters like "="
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
@@ -36,7 +36,9 @@ public class CustomController {
 
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
-        CustomFood food = gson.fromJson(json, CustomFood.class);
+        CustomFoodRequest req = gson.fromJson(json, CustomFoodRequest.class);
+
+        CustomFood food = req.getItem()[0];
 
         customRepository.queryCreateCustomFood(food.getName(), food.getCalories(), food.getFat(), food.getCarbs(), food.getProtein());
     }
---
+++ Backend/backend/src/main/java/com/requests/backend/models/requests/CustomFoodRequest.java
@@ -0,0 +1,18 @@
+package com.requests.backend.models.requests;
+
+import com.requests.backend.models.CustomFood;
+
+public class CustomFoodRequest {
+    private CustomFood[] item;
+
+    public CustomFoodRequest() {
+    }
+
+    public CustomFoodRequest(CustomFood[] item) {
+        this.item = item;
+    }
+
+    public CustomFood[] getItem() {
+        return item;
+    }
+}
