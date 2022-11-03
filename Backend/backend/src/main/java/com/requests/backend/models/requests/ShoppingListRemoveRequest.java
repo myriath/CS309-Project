7ae6f342a77e0cf2acabadc1c6684e2aca0db435
jorgoose <your.email@goes.here>
2022-11-02 23:59:25 -0500

    Updated ShoppingListRemoveRequest to use correct variable name (description rather than itemName)

--- Backend/backend/src/main/java/com/requests/backend/models/requests/ShoppingListRemoveRequest.java
+++ Backend/backend/src/main/java/com/requests/backend/models/requests/ShoppingListRemoveRequest.java
@@ -3,24 +3,15 @@ package com.requests.backend.models.requests;
 import com.requests.backend.models.AuthModel;
 
 public class ShoppingListRemoveRequest {
-    private String itemName;
+    private String description;
 
-    // TODO: Replace with token
     private String token;
 
     public String getItemName() {
-        return this.itemName;
+        return this.description;
     }
 
     public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
+        this.description = itemName;
     }
 }
