fa6a50a110fee351f9224409f685fa1909998879
mitch <mitchhudson2002@gmail.com>
2022-11-03 18:01:55 -0500

    Temp fix for upc stuff

---
+++ Backend/backend/src/main/java/com/requests/backend/models/UPC.java
@@ -0,0 +1,4 @@
+package com.requests.backend.models;
+
+public class UPC {
+}
--- Backend/backend/src/main/java/com/requests/backend/repositories/UPCRepository.java
+++ Backend/backend/src/main/java/com/requests/backend/repositories/UPCRepository.java
@@ -1,11 +1,11 @@
 package com.requests.backend.repositories;
 
-import com.requests.backend.models.UPC;
+//import com.requests.backend.models.UPC;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UPCRepository extends JpaRepository<UPC, Integer> {
+public interface UPCRepository extends JpaRepository<String, Integer> {
 
     @Query (
             value = "SELECT fdc_id FROM upc WHERE upc = :upc",
