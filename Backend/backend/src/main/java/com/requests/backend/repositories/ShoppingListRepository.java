04509758b46b281fa1728dee798369d66059da9f
jorgoose <your.email@goes.here>
2022-11-03 00:13:54 -0500

    Hotfix - ShoppingListController Custom Food

--- Backend/backend/src/main/java/com/requests/backend/repositories/ShoppingListRepository.java
+++ Backend/backend/src/main/java/com/requests/backend/repositories/ShoppingListRepository.java
@@ -17,10 +17,10 @@ public interface ShoppingListRepository extends JpaRepository<ShoppingList, Inte
     @Modifying
     @Query(
             value =
-                    "INSERT INTO shopping_list (id, username, description, stricken, is_custom) VALUES (:fdc_id, :username, :description, :stricken, :isCustom)",
+                    "INSERT INTO shopping_list (description, username, id, is_custom, stricken) VALUES (:description, :username, :id, :is_custom, :stricken)",
             nativeQuery = true)
     @Transactional
-    void queryCreateShoppingListEntry(@Param("username") String username, @Param("description") String description, @Param("fdc_id") Integer fdcId, @Param("stricken") Boolean stricken, @Param("isCustom") Boolean isCustom);
+    void queryCreateShoppingListEntry(@Param("username") String username, @Param("description") String description, @Param("id") Integer id, @Param("stricken") Boolean stricken, @Param("is_custom") Boolean isCustom);
 
     @Modifying
     @Query(
