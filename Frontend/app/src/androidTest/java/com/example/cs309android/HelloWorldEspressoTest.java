02ac2660b71bf3760916d1b0776386f7b43db76b
myriath <wmhudson@iastate.edu>
2022-11-22 00:17:14 -0600

    Created basic unit test for espresso
    - Fails because it is not a real one

---
+++ Frontend/app/src/androidTest/java/com/example/cs309android/HelloWorldEspressoTest.java
@@ -0,0 +1,29 @@
+package com.example.cs309android;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.cs309android.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloWorldEspressoTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void test() {
+        onView(withText("Hello World!")).check(matches(isDisplayed()));
+    }
+}
