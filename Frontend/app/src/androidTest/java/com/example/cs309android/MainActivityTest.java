dee34e3b989fe685775d6deba0aede4dbc186a8f
myriath <wmhudson@iastate.edu>
2022-11-23 15:41:26 -0600

    Wrote some tests
    - Wrote 3 login tests
    - Added testing utils for various utilities while testing
      - ex: textinputlayout handling and getting the globalclass

--- Frontend/app/src/androidTest/java/com/example/cs309android/MainActivityTest.java
+++ Frontend/app/src/androidTest/java/com/example/cs309android/MainActivityTest.java
@@ -1,9 +1,8 @@
 package com.example.cs309android;
 
 import static androidx.test.espresso.Espresso.*;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -17,13 +16,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class HelloWorldEspressoTest {
+public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> rule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void test() {
+
         onView(withText("Hello World!")).check(matches(isDisplayed()));
     }
 }
