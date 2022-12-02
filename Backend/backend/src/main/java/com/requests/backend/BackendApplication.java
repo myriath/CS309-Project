0d2a294cc48b0b16cd011abd5f21d88cf220e265
myriath <mitchhudson2002@gmail.com>
2022-12-02 10:37:47 -0600

    PLEASE REMOVE JACKSON >:(

--- Backend/backend/src/main/java/com/requests/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/requests/backend/BackendApplication.java
@@ -2,11 +2,12 @@ package com.requests.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, JacksonAutoConfiguration.class })
 @ComponentScan(basePackages = "com.requests.backend.controllers")
 public class BackendApplication {
 
