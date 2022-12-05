cdf9c74dcdd2d4e764ebe2ae50cdb69398a10ab6
mitch <mitchhudson2002@gmail.com>
2022-12-05 15:56:59 -0600

    test fix for websocket 3

--- Backend/backend/src/main/java/com/requests/backend/WebsocketConfig.java
+++ Backend/backend/src/main/java/com/requests/backend/WebsocketConfig.java
@@ -2,15 +2,18 @@ package com.requests.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
- * Security config to enable HTTPS and disable CORS
- * EXPERIMENT 5
+ * Config for enabling websockets
  *
  * @author Mitch Hudson
  */
 @Configuration
+@EnableWebSocket
 public class WebsocketConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
